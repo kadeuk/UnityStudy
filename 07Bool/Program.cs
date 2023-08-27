19a3668bcb1f4ea2a418dcfa688f884c7ec782bb
kadeuk <gomsonjangin@gmail.com>
2023-08-27 23:18:51 +0900

    Add switch

--- 07Bool/Program.cs
+++ 07Bool/Program.cs
@@ -23,6 +23,50 @@
 //Console.WriteLine(pangram.Contains("fox")  == false);
 //Console.WriteLine(!pangram.Contains("fox"));
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(!pangram.Contains("fox"));
+//Console.WriteLine(!pangram.Contains("cow"));
+
+//int saleAmount = 1001;
+////int discount = saleAmount > 1000 ? 100 : 50;
+//Console.WriteLine($"Discount: {(saleAmount >1000 ? 100 : 50)}");
+
+//Random coin = new Random();
+//int flip = coin.Next(0, 2);
+//Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+//Random conin = new Random();
+//int flip2 = coin.Next(0, 3);
+//Console.WriteLine((flip2 == 0)? "True" : "False");
+
+using System.Net.Security;
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager")) 
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
