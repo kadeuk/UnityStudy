09a71868a1fd799122922c96674ad42a13d725a7
kadeuk <gomsonjangin@gmail.com>
2023-09-03 09:55:57 +0900

    Add ourAnimals

---
+++ 16OurAnimals/OurAnimals.cs
@@ -0,0 +1,11 @@
+﻿
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
